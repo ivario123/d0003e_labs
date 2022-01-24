adb663cbb022dfb36fc8688c663e39b439a14797
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2022-01-24 15:30:54 +0100

    added numbers

--- lab1/lab1/lab1/include/dictionary.h
+++ lab1/lab1/lab1/include/dictionary.h
@@ -10,8 +10,16 @@
 #define DICTIONARY_H_
 
 enum dictionary{
-	A_LSB = 0x51,
-	A_MSB = 0x0F
+	ZERO = 0x1551
+	ONE = 0x0110
+	TWO = 0x1E11
+	THREE = 0x1B11
+	FOUR = 0x0B50
+	FIVE = 0x1B41
+	SIX = 0x1F41
+	SEVEN = 0x0111
+	EIGHT = 0x1F51
+	NINE = 0x1B51
 	};
 
 
